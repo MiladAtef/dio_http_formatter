88d775a6bed6c21abcfb169dabb003d5ff78e7b5
Milad Atef <meladattef2017@gmail.com>
2023-02-23 10:19:19 +0200

    Update dio and replace pedantic with lints.

--- example/dio_http_formatter_example.dart
+++ example/dio_http_formatter_example.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:dio_http_formatter/dio_http_formatter.dart';
 
 void main() async {
-  final _dio = Dio();
+  final dio = Dio();
 
-  _dio.interceptors.add(HttpFormatter());
+  dio.interceptors.add(HttpFormatter());
 
-  await _dio.get('https://example.com');
+  await dio.get('https://example.com');
 }
